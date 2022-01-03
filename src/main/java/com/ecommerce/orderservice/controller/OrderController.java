e5dec5b730e7cbf7036e08174e56b46e21f80d99
ravindar86 <ravindar.mca@gmail.com>
2022-01-03 16:15:23 +0530

    Added changes for Circuit Breaker (Resilience4j), log trace (Sleuth&Zipkin) and logstash (ELK stack)

--- src/main/java/com/ecommerce/orderservice/controller/OrderController.java
+++ src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -6,12 +6,18 @@ import com.ecommerce.orderservice.model.Order;
 import com.ecommerce.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api/order")
@@ -21,12 +27,27 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final StreamBridge streamBridge;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+    private final ExecutorService traceableExecutorService;
 
     @PostMapping
     public String placeOrder(@RequestBody OrderDto orderDto){
 
-        boolean isStockAvailable = orderDto.getOrderLineItemsList().stream()
+        circuitBreakerFactory.configureExecutorService(traceableExecutorService);
+
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+        Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItemsList().stream()
+                .allMatch(orderLineItems -> {
+                    log.info("Making call to Inventory Service for SkuCode ->",orderLineItems.getSkuCode());
+                    return inventoryClient.checkStock(orderLineItems.getSkuCode());
+                });
+        boolean isStockAvailable = circuitBreaker.run(booleanSupplier,throwable -> handleErrorCase());
+
+       /* boolean isStockAvailable = orderDto.getOrderLineItemsList().stream()
                                 .allMatch(orderLineItems -> inventoryClient.checkStock(orderLineItems.getSkuCode()));
+        */
+
 
         if(isStockAvailable) {
             Order order = new Order();
@@ -34,10 +55,19 @@ public class OrderController {
             order.setOrderNumber(UUID.randomUUID().toString());
 
             orderRepository.save(order);
+
+            log.info("Sending order details with order id {} to notification service", order.getId());
+
+            streamBridge.send("notificationEventSupplier-out-0", MessageBuilder.withPayload(order.getId()).build());
+
             return "Order Placed Successfully!";
         }
         else {
             return "Product not in Stock for the order!";
         }
+    }
+
+    private Boolean handleErrorCase() {
+        return false;
     }
 }
--- src/main/java/com/ecommerce/orderservice/model/OrderLineItems.java
+++ src/main/java/com/ecommerce/orderservice/model/OrderLineItems.java
@@ -21,5 +21,5 @@ public class OrderLineItems {
     private Long id;
     private String skuCode;
     private BigDecimal price;
-    private Integer qunatity;
+    private Integer quantity;
 }
